41037c460386e099c187ef1e716743353cd036c4
grazielegs <graziwho@gmail.com>
2019-05-11 15:46:19 -0300

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';  
-import { NgModule } from '@angular/core';  
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { RootComponent } from './root/root.component';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
 
 @NgModule({
-  declarations: [
-    RootComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [RootComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { } 
+export class AppModule { }
